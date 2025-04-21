e1b3244c4d3b37eb1efc1d2491ca3b50ab769ed7
Richard Knoll <riknoll@users.noreply.github.com>
2025-04-21 16:38:15 -0700

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,22 @@
-
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+	
+})
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . 2 2 2 1 . . . . . . . . 
+    . . . . 2 2 2 2 2 2 . . . . . . 
+    . . . . d f d f . . . . . . . . 
+    . . . . d d d d d . . . . . . . 
+    . . . . d d e e e . . . . . . . 
+    . . . . d d d d . . . . . . . . 
+    . . 2 8 2 2 2 2 8 2 . . . . . . 
+    . 2 . 8 2 2 2 2 8 . 2 . . . . . 
+    . 2 . 8 2 2 2 2 8 . 2 . . . . . 
+    . 2 . 5 2 2 2 2 5 . 2 . . . . . 
+    . d . 8 8 8 8 8 8 . d . . . . . 
+    . . . 8 8 8 8 8 8 . . . . . . . 
+    . . . 8 . . . . 8 . . . . . . . 
+    . . . 8 . . . . 8 . . . . . . . 
+    . . . e e . . . e e . . . . . . 
+    `, SpriteKind.Player)
+tiles.setCurrentTilemap(tilemap`level1`)
